c613e8babd7134e27cb114415e45214fa51394bd
vitor <vitorrodriguesjc@gmail.com>
2022-02-16 15:19:44 -0300

    Added comments

--- productAPI/server/routes/productCreate.js
+++ productAPI/server/routes/productCreate.js
@@ -2,15 +2,13 @@ const express = require("express");
 const Product = require("../entities/productEntity");
 const router = express.Router();
 
-router.get("/view", (req, res) => {
-    res.send("Teste");
-})
-
 router.post('/create', async (req, res) => {
     try {
-        if (await Product.findOne(req.body)) {
+        // verifies if the product already exists, or if the name already has been registered
+        if (await Product.findOne(req.body) || await Product.findOne(req.body.name)) {
             return res.status(400).send("Error: A product with these expecifications already exists");
         }
+        // send the product to the mongoDB
         const product = await Product.create(req.body);
 
         return res.send( {product} );
